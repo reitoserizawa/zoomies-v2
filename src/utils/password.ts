3d7fa6138c933a8cad349ada2c222e05a3530071
ReitoSerizawa <s.reitiger@gmail.com>
2024-10-30 13:20:40 -0400

    added password util

---
+++ src/utils/password.ts
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt';
+
+class PasswordUtil {
+    salt_rounds: number;
+
+    constructor(public password: string) {
+        this.salt_rounds = parseInt(process.env.SALT_ROUNDS);
+    }
+
+    async hash(): Promise<string> {
+        const hahsed_password = await bcrypt.hash(
+            this.password,
+            this.salt_rounds
+        );
+
+        return hahsed_password;
+    }
+
+    async verify(hash: string): Promise<boolean> {
+        const is_match = await bcrypt.compare(this.password, hash);
+
+        return is_match;
+    }
+}
+
+export default PasswordUtil;
