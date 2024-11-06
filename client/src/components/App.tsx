ed0b17bbc4c9a48be1cf036331ba3bc1f7012d66
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-06 12:30:35 -0500

    added global style

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+
 import { store } from '../redux/store';
+
+import Main from './Main';
+
+import { GlobalStyle } from '../ui/index.styles';
 
 function App() {
     return (
         <Provider store={store}>
-            <div></div>
+            <GlobalStyle />
+            <Main />
         </Provider>
     );
 }
