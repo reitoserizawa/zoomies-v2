f419e4df05024806c9cd5e81f0bbcfc0773034c0
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-06 13:10:08 -0500

    added img

---
+++ client/src/images/index.ts
@@ -0,0 +1,11 @@
+import dogPaw from './paw.png';
+
+export const DogPawImg = {
+    src: dogPaw,
+    alt: 'Dog Paw',
+    size: {
+        small: '50px',
+        midium: '100px',
+        big: '150px'
+    }
+};
