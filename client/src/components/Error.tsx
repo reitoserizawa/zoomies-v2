e81bb934782e51b398c4bfa122446a59c62fa56c
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-11 15:10:50 -0500

    added default error message

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -4,7 +4,7 @@ import { P } from '../ui/heading.styles';
 
 import { ErrorState } from '../interfaces/error';
 
-const Error: React.FC<ErrorState> = ({ message }) => {
+const Error: React.FC<ErrorState> = ({ message = 'Unknown error occured' }) => {
     return (
         <P color='red' margin='0px'>
             {message}
