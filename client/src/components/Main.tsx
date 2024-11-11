3511b298264bfc710103d7d6e09119131cb1e582
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-11 13:12:34 -0500

    added router

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import LogIn from './LogIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const Main = () => {
-    return <LogIn />;
+import LogIn from './LogIn';
+import Register from './Register';
+import Profile from './Profile';
+import RequiredLogin from './RequiredLogIn';
+
+const Main: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path={'/'}
+                    element={
+                        <RequiredLogin>
+                            <Profile />
+                        </RequiredLogin>
+                    }
+                />
+                <Route path={'/login'} element={<LogIn />} />
+                <Route path={'/register'} element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default Main;
