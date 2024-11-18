1652898078d3063d44f29113ce572b7e2ccdf81c
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-18 14:13:39 -0500

    added get all dog parks query

--- client/src/redux/reducers/protected-api-slice.ts
+++ client/src/redux/reducers/protected-api-slice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { UserLogInRequest, UserState } from '../../states/user';
 import { PetCreateState, PetState } from '../../states/pet';
+import { DogParkState } from '../../states/dog-park';
 
 export const protectedApiSlice = createApi({
     reducerPath: 'protectedApiSlice',
@@ -61,8 +62,13 @@ export const protectedApiSlice = createApi({
                 url: `pets/${id}`,
                 method: 'DELETE'
             })
+        }),
+        getAllDogParks: builder.query<DogParkState[], null>({
+            query: () => ({
+                url: `dog-parks`
+            })
         })
     })
 });
 
-export const { useLogInUserMutation, useGetUserDetailsQuery, useCreatePetMutation, useUpdatePetDetailsMutation, useDeletePetMutation } = protectedApiSlice;
+export const { useLogInUserMutation, useGetUserDetailsQuery, useCreatePetMutation, useUpdatePetDetailsMutation, useDeletePetMutation, useGetAllDogParksQuery } = protectedApiSlice;
--- src/routes/protected/index.ts
+++ src/routes/protected/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import userRoutes from './users';
 import petRoutes from './pets';
+import dogParkRoutes from './dog-park';
 
 const protetdRoutes = Router();
 
 protetdRoutes.use('/users', userRoutes);
 protetdRoutes.use('/pets', petRoutes);
+protetdRoutes.use('/dog-parks', dogParkRoutes);
 
 export default protetdRoutes;
