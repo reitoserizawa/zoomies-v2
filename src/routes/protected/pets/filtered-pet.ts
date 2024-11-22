9a631778427862eb12cb5fa6a7986e96fc1138e1
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-21 19:03:40 -0500

    added unchecked in pets route

---
+++ src/routes/protected/pets/filtered-pet.ts
@@ -0,0 +1,25 @@
+import { CustomRequest, NextFunction, Response } from 'express';
+
+import User from '../../../models/user';
+import Pet from '../../../models/pet';
+
+export const getUncheckedInPets = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.fromJwtPayload(req);
+        user.setPets();
+
+        const pets: Pet[] = user?.pets || [];
+        const pets_without_active_check_ins = await Promise.all(
+            pets.map(async pet => {
+                const has_active_check_in = await pet.hasActiveCheckIn();
+                return { pet, has_active_check_in };
+            })
+        );
+
+        const filtered_pets = pets_without_active_check_ins.filter(result => !result.has_active_check_in).map(result => result.pet);
+
+        res.json(await Promise.all(filtered_pets.map(pet => pet.prepareForCollection())));
+    } catch (err) {
+        next(err);
+    }
+};
