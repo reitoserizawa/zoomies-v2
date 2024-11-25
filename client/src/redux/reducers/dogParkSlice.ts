936adad5e4db818c4588f5cbf0084f40bd5a444f
ReitoSerizawa <s.reitiger@gmail.com>
2024-11-25 13:11:57 -0500

    added dog park slice

---
+++ client/src/redux/reducers/dogParkSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { DogParkState } from '../../states/dog-park';
+
+const initialState: DogParkState = {
+    name: '',
+    address: '',
+
+    type: null,
+    geo: null,
+
+    check_ins: undefined,
+    active_check_ins: undefined
+};
+
+const dogParkSlice = createSlice({
+    name: 'dogPark',
+    initialState,
+    reducers: {
+        setDogParkDetails: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            };
+        },
+        setActiveCheckIns: (state, action) => {
+            return {
+                ...state,
+                active_check_ins: { ...action.payload }
+            };
+        }
+    }
+});
+
+export const { setDogParkDetails, setActiveCheckIns } = dogParkSlice.actions;
+
+export default dogParkSlice.reducer;
