9715b2afe03af9588959ef115b1d6d3e5217c7c2
ReitoSerizawa <s.reitiger@gmail.com>
2024-12-10 17:03:24 -0500

    seeding debugged

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -2,45 +2,72 @@ import NodeGeocoder from 'node-geocoder';
 
 import nyc_dog_park_data from './data/dog-park-data';
 import PrismaClientModel from '../models/prisma-client';
+import { Prisma } from '@prisma/client';
 
 const main = async () => {
-    // const prisma_client = PrismaClientModel.prisma;
-    // const dog_park_names = nyc_dog_park_data.filter(park => park.Name).map(park => park.Name);
-    // const options = {
-    //     provider: 'google' as const,
-    //     apiKey: 'AIzaSyD2UFZE6Ks2ajO8keOYXbfhARqRgI37C54',
-    //     formatter: null
-    // };
-    // const geocoder = NodeGeocoder(options);
-    // for (let name of dog_park_names) {
-    //     const result = await geocoder.geocode(name);
-    //     const full_address = result[0].formattedAddress;
-    //     const address = result[0].formattedAddress?.split(',')[0];
-    //     const city = result[0].formattedAddress?.split(',')[1];
-    //     const district = result[0].administrativeLevels?.level1short;
-    //     const postal_code = result[0].zipcode;
-    //     const country = result[0].countryCode;
-    //     const latitude = result[0].latitude;
-    //     const longitude = result[0].longitude;
-    //     if (!full_address || !city || !district || !address || !postal_code || !country || !latitude || !longitude) {
-    //         continue;
-    //     }
-    //     await prisma_client.address.upsert({
-    //         where: {
-    //             full_address: full_address
-    //         },
-    //         update: {
-    //             name: dog_park_props.PROPERTY_NAME ? dog_park_props.PROPERTY_NAME : dog_park_props.DP_NAME,
-    //             address: dog_park_props.ADDRESS,
-    //             geo: dog_park_geo.coordinates
-    //         },
-    //         create: {
-    //             name: dog_park_props.PROPERTY_NAME ? dog_park_props.PROPERTY_NAME : dog_park_props.DP_NAME,
-    //             address: dog_park_props.ADDRESS,
-    //             geo: dog_park_geo.coordinates
-    //         }
-    //     });
-    // }
+    const prisma_client = PrismaClientModel.prisma;
+
+    const dog_park_items = nyc_dog_park_data
+        .filter(park => park.Name && park.Notes && park.DogRuns_Type)
+        .map(park => {
+            return {
+                name: park.Name,
+                notes: park.Notes,
+                type: park.DogRuns_Type
+            };
+        });
+
+    const options = {
+        provider: 'google' as const,
+        apiKey: 'AIzaSyD2UFZE6Ks2ajO8keOYXbfhARqRgI37C54',
+        formatter: null
+    };
+    const geocoder = NodeGeocoder(options);
+
+    for (let dog_park_item of dog_park_items) {
+        const result = await geocoder.geocode(dog_park_item.name);
+
+        const full_address = result[0].formattedAddress;
+        const address = result[0].formattedAddress?.split(',')[0];
+        const city = result[0].formattedAddress?.split(',')[1];
+        const district = result[0].administrativeLevels?.level1short;
+        const postal_code = result[0].zipcode;
+        const country = result[0].countryCode;
+        const latitude = result[0].latitude;
+        const longitude = result[0].longitude;
+
+        if (!full_address || !city || !district || !address || !postal_code || !country || !latitude || !longitude) {
+            continue;
+        }
+
+        const address_item = await prisma_client.address.create({
+            data: {
+                full_address,
+                address,
+                city,
+                district,
+                postal_code,
+                country,
+                latitude,
+                longitude
+            }
+        });
+
+        const address_where_input = Prisma.validator<Prisma.AddressWhereInput>()({
+            id: address_item.id
+        });
+
+        await prisma_client.dogPark.create({
+            data: {
+                name: dog_park_item.name,
+                address: {
+                    connect: address_where_input
+                },
+                type: dog_park_item.type,
+                notes: dog_park_item.notes
+            }
+        });
+    }
 };
 
 main()
