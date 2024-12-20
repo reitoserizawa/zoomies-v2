026ee9c2f27eb1c362914d3a1c7579a86cdab5b0
ReitoSerizawa <s.reitiger@gmail.com>
2024-12-20 16:14:19 -0500

    added profile settings content comp

---
+++ client/src/components/Dashboard/ProfileSettings/ProfileSettingsSectionContent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { FlexContainer } from '../../../ui/container.styles';
+
+// import Loader from '../../Loader';
+
+const ProfileSettingsSectionContent: React.FC = () => {
+    // if (fetchingRecentDogParkCheckIns) {
+    //     return (
+    //         <div style={{ height: '350px' }}>
+    //             <Loader />
+    //         </div>
+    //     );
+    // }
+
+    return <FlexContainer $alignItems='flex-start' $justifyContent='flex-start' $gap='15px' style={{ padding: '0px 16px 16px' }}></FlexContainer>;
+};
+
+export default ProfileSettingsSectionContent;
